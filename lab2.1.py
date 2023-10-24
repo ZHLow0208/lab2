5c676898995d1a53b662e4caeee5294a1be93d67
ZhengHong Low <zhlow.22@ichat.sp.edu.sg>
2023-10-24 15:54:15 +0800

    updated with statistic

--- lab2.1.py
+++ lab2.1.py
@@ -1,4 +1,4 @@
-
+import statistics as st
 
 def main():
  print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -13,7 +13,7 @@ def display_main_menu():
  print("Enter some numbers separated by commas (e.g. 5, 67,32)")
 
 def calc_average(x):
- y = sum(x)/len(x)
+ y = st.mean(x)
  return y
 
 def get_user_input():
@@ -27,12 +27,8 @@ def sort_temperature(x):
  y = sorted(x,reverse=False)
  return y
 def calc_median_temperature(x):
- if len(x) % 2 == 1:
-  median = x[len(x) // 2]
- else:
-  mid = len(x) // 2
-  median = (int(x[mid - 1]) + int(x[mid])) / 2
- return median
+ num_median = st.median(x)
+ return num_median
 
 if __name__ == "__main__":
  main()
