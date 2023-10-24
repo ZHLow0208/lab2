e33cce9842c19e1ed8e582252a266da75a5cd766
ZhengHong Low <zhlow.22@ichat.sp.edu.sg>
2023-10-24 15:43:04 +0800

    Lab2_v1.1

--- lab2.py
+++ lab2.py
@@ -1,1 +1,38 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def main():
+ print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+ display_main_menu()
+ num_list = [float(item) for item in get_user_input()]
+ print(find_min_max(num_list))
+ print(sort_temperature(num_list))
+ print(calc_median_temperature(num_list))
+ print(calc_average(num_list))
+
+def display_main_menu():
+ print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+
+def calc_average(x):
+ y = sum(x)/len(x)
+ return y
+
+def get_user_input():
+ x = input().split(",")
+ return x
+def find_min_max(x):
+ max_temp = max(x)
+ min_temp = min(x)
+ return [max_temp,min_temp]
+def sort_temperature(x):
+ y = sorted(x,reverse=False)
+ return y
+def calc_median_temperature(x):
+ if len(x) % 2 == 1:
+  median = x[len(x) // 2]
+ else:
+  mid = len(x) // 2
+  median = (int(x[mid - 1]) + int(x[mid])) / 2
+ return median
+
+if __name__ == "__main__":
+ main()
