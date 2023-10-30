6f65053b678dfeaaa1143dcb03b67a289f004a3f
ZhengHong Low <zhlow.22@ichat.sp.edu.sg>
2023-10-30 16:34:11 +0800

    updated based on Lab 3 to return value

--- bmi.py
+++ bmi.py
@@ -3,4 +3,10 @@ def calculate_bmi(height, weight):
  print("Weight = " + str(weight))
  bmi = weight/height**2
  print(str(bmi))
+ if bmi < 18.5:
+  print("-1")
+ elif bmi > 25:
+  print("1")
+ else:
+  print("0")
 calculate_bmi(weight=57, height=1.73)
